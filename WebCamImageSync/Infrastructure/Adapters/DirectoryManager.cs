281bc1de09ba849923f2607647cc2c755dbcff25
abbirku <abbir.ku@gmail.com>
2021-08-09 16:25:18 +0600

    Create Infrastructure class library and DirectoryManager adapter

---
+++ WebCamImageSync/Infrastructure/Adapters/DirectoryManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Infrastructure.Adapters
+{
+    public interface IDirectoryManager
+    {
+        bool Exists(string directory);
+        void CreateDirectory(string directory);
+    }
+
+    public class DirectoryManager : IDirectoryManager
+    {
+        public void CreateDirectory(string directory) => Directory.CreateDirectory(directory);
+        public bool Exists(string directory) => Directory.Exists(directory);
+    }
+}
