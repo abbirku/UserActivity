81aa65cbc54e631ffceaf331c7020d30f7afd9f7
abbirku <abbir.ku@gmail.com>
2021-08-09 17:22:00 +0600

    Create DirectoryManagerService

--- WebCamImageSync/Infrastructure/DirectoryManager/DirectoryManagerService.cs
+++ WebCamImageSync/Infrastructure/DirectoryManager/DirectoryManagerService.cs
@@ -1,23 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Infrastructure.DirectoryManager
 {
     public interface IDirectoryManagerService
     {
+        bool ChecknCreateDirectory(string directoryPath);
         string GetProgramDataDirectoryPath(string appFolder);
-        string ChecknCreateDirectory(string folderName);
-        string CreateFilePath(string folderName, string fileName);
+        string CreateProgramDataFilePath(string folderName, string fileName);
     }
 
     public class DirectoryManagerService : IDirectoryManagerService
     {
         private readonly IDirectoryManagerAdapter _directoryManagerAdapter;
 
-        public DirectoryManagerService(IDirectoryManagerAdapter directoryManagerAdapter) 
+        public DirectoryManagerService(IDirectoryManagerAdapter directoryManagerAdapter)
             => _directoryManagerAdapter = directoryManagerAdapter;
 
+        /// <summary>
+        /// Given a directory path of a folder it creates the folder under the directory if not exists
+        /// </summary>
+        public bool ChecknCreateDirectory(string directoryPath)
+        {
+            if (!_directoryManagerAdapter.Exists(directoryPath))
+            {
+                _directoryManagerAdapter.CreateDirectory(directoryPath);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Create a file path of given folder name which is under C:\ProgramData
+        /// </summary>
         public string GetProgramDataDirectoryPath(string appFolder)
         {
             if (string.IsNullOrWhiteSpace(appFolder))
@@ -26,25 +41,18 @@ namespace Infrastructure.DirectoryManager
             return $"{_directoryManagerAdapter.CommonApplicationPath}\\{appFolder}";
         }
 
-        public string ChecknCreateDirectory(string folderName)
+        /// <summary>
+        /// Create a file path under given folderName in C:\ProgramData
+        /// </summary>
+        public string CreateProgramDataFilePath(string folderName, string fileName)
         {
+            if (string.IsNullOrWhiteSpace(folderName) || string.IsNullOrWhiteSpace(fileName))
+                throw new Exception("Given folder or File name is empty.");
+
             var directory = GetProgramDataDirectoryPath(folderName);
+            var isCreated = ChecknCreateDirectory(directory);
 
-            if (!_directoryManagerAdapter.Exists(directory))
-                _directoryManagerAdapter.CreateDirectory(directory);
-
-            return directory;
-        }
-
-        public string CreateFilePath(string folderName, string fileName)
-        {
-            if (string.IsNullOrWhiteSpace(fileName))
-                throw new Exception("File name is empty.");
-
-            var directory = ChecknCreateDirectory(folderName);
-            var filePath = $"{directory}\\{fileName}";
-
-            return filePath;
+            return isCreated ? $"{directory}\\{fileName}" : string.Empty;
         }
     }
 }
