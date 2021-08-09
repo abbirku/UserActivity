cba2ac2d13a155c9ec24b1c2f00602c244f9c8f1
abbirku <abbir.ku@gmail.com>
2021-08-09 15:50:35 +0600

    Initial commit

---
+++ WebCamImageSync/WebCamImageSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebCamImageSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
