6f2d5968c2bfa4bad884a2dc77a0bb4477a01392
abbirku <abbir.ku@gmail.com>
2021-08-12 20:21:27 +0600

    Remove unnecessary injection

--- WebCamImageSync/WebCamImageSync/Application.cs
+++ WebCamImageSync/WebCamImageSync/Application.cs
@@ -10,16 +10,13 @@ namespace WebCamImageSync
     public class Application
     {
         private readonly IEgmaCvAdapter _egmaCvAdapter;
-        private readonly IFileManagerService _fileManagerService;
         private readonly IDirectoryManagerService _directoryManagerService;
         private string _folderName;
 
         public Application(IEgmaCvAdapter egmaCvAdapter, 
-            IFileManagerService fileManagerService,
             IDirectoryManagerService directoryManagerService)
         {
             _egmaCvAdapter = egmaCvAdapter;
-            _fileManagerService = fileManagerService;
             _directoryManagerService = directoryManagerService;
             _folderName = AppSettingsInfo.GetCurrentValue<string>("FolderName");
         }
